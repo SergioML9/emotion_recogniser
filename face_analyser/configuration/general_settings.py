b8075653d4d847733ded1faacedd8b0cf24b239d
Sergio Muñoz <tito.9.24@gmail.com>
2017-07-01 02:10:32 +0200

    weights name change

--- face_analyser/configuration/general_settings.py
+++ face_analyser/configuration/general_settings.py
@@ -2,5 +2,5 @@
 detected_emotions = ['Angry', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 emotions_values = {'Angry': 0, 'Disgust': 1, 'Fear': 2, 'Happy': 3,
            'Sad': 4, 'Surprise': 5, 'Neutral': 6}
-model_weights_path = 'model/weights/my_model_weights_83.h5'
+model_weights_path = 'model/weights/fer2013_weights.h5'
 verbose = True
